fd3561b294a0d8b7465f8844667bd612d35a7403
ak22021990-jpg <ak22021990@gmail.com>
2026-05-15 16:29:53 +0530

    modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // Replace with your deployed Google Apps Script Web App URL
-export const LEADERBOARD_URL = 'https://script.google.com/macros/s/AKfycbx_ytMictYDbd16rq6fGCgFKE9NzqtwyWZkDT9Lkq50lF3tsakpar14uiFj3i6UbMDT_A/exec';
+export const LEADERBOARD_URL = 'https://script.google.com/macros/s/AKfycbz5lamDtRDa8TYx6cWDZdnPbkDooC-S7OKeCayXkXHUcM9K3h27HtOMAZdtQTgnAscT/exec';
